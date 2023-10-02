93bdb35c8e0b322ad0ab16208c0d98b5b58c88b9
Kate test <d.swetlov2017@gmail.com>
2023-10-02 18:19:19 +0300

    modified

--- src/main/java/ru/netology/services/VacationService.java
+++ src/main/java/ru/netology/services/VacationService.java
@@ -8,7 +8,7 @@ public class VacationService {
         for (int month = 0; month < 12; month++) {
             if (money > threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = money - expenses - expenses - expenses - expenses;
+                money = (money - expenses) / 3;
             } else {
                 money = money + income - expenses;
             }
